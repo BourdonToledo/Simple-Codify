c9e8181be26b6d39a6f4ca10a1872d0a5fb4dac5
BourdonToledo <58145074+BourdonToledo@users.noreply.github.com>
2023-03-28 18:32:51 -0300

    Add files via upload

---
+++ Trabalho2.asm
@@ -0,0 +1,44 @@
+.data
+	
+	Endereço:	.asciiz "C:/Users/André/Desktop/tate.txt"	#endereço do arquivo que tem os numeros
+	Buffer:		.space 1024
+	Buffer2:	.space 1024
+	
+.text
+
+	addi $v0, $zero, 8
+	la $a0, Buffer2	
+	addi $a1, $zero, 1024
+	syscall
+	
+	
+	
+	#addi $v0, $zero, 13
+	#la $a0, Endereço	
+	#addi $a1, $zero, 0
+	
+	#syscall
+	
+	#move $a0, $v0
+	#la $a1, Buffer
+	#addi $a2, $zero, 1024
+	#addi $v0, $zero, 14
+	
+	#syscall
+	
+	
+	la $a0, Buffer2
+	#move $a0, $a1
+	#lw $t0, ($a0)
+	#addi $t0, $t0, 5
+	#sw $t0, ($a0)
+	addi $v0, $zero, 4
+	addi $a1, $zero, 1024
+	syscall
+	
+	#li $v0, 16  # $a0 already has the file descriptor
+	#syscall
+	
+	addi $v0, $zero, 10  # $a0 already has the file descriptor
+	syscall
+	
